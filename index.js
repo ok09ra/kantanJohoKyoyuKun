0f99dd1a272d3b8be0aa2b4761915f0d71332973
ok09ra <okudasota@gmail.com>
2021-04-02 16:05:08 +0900

    bug fix

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ written by Sota Okuda
 */
 
 
-const server = require("express");
-const line = require("@line/bot-sdk");
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+// -----------------------------------------------------------------------------
+// パラメータ設定
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
