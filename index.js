8e6a9994e14be03733e6d986ee02b2465a84bc37
ok09ra <okudasota@gmail.com>
2021-04-02 15:40:40 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+ref
+https://qiita.com/nkjm/items/38808bbc97d6927837cd
+
+2021/04/02
+written by Sota Okuda
+*/
+
+
+const server = require("express");
+const line = require("@line/bot-sdk");
+
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
+
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
